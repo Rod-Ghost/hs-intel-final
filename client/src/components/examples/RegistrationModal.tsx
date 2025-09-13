c82d42f87989c3cdbd46701747e35c48df433afe
Rod-Ghost <ghosty.teix@gmail.com>
2025-09-13 11:05:33 -0300

    Add client/src/components/examples/RegistrationModal.tsx

---
+++ client/src/components/examples/RegistrationModal.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { Button } from "@/components/ui/button";
+import RegistrationModal from '../RegistrationModal';
+
+export default function RegistrationModalExample() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="p-8">
+      <Button onClick={() => setIsOpen(true)}>
+        Open Registration Modal
+      </Button>
+      <RegistrationModal 
+        isOpen={isOpen} 
+        onClose={() => setIsOpen(false)} 
+      />
+    </div>
+  );
+}
